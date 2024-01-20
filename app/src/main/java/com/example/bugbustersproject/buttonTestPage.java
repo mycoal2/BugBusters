bb71b4cc201d06db097468b276e41be6efcd6519
mycoal2 <michaelhong136@gmail.com>
2024-01-20 13:27:11 -0500

    google maps implemented in testPage

--- app/src/main/java/com/example/bugbustersproject/buttonTestPage.java
+++ app/src/main/java/com/example/bugbustersproject/buttonTestPage.java
@@ -5,15 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class buttonTestPage extends AppCompatActivity {
+import android.widget.Button;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class buttonTestPage extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_test_page);
         setupUI();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Customize the map if needed
     }
     protected void setupUI() {
         // Define Button
