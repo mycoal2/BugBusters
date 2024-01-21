a70f323a6817706c5f6413e67609ab78da39001c
DanielCheishvili <60618343+DanielCheishvili@users.noreply.github.com>
2024-01-21 02:01:44 -0500

    Map API address autocomplete done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,9 @@ android {
 }
 
 dependencies {
-    implementation ("com.squareup.okhttp3:okhttp:4.9.1") // For making HTTP requests
-    implementation ("com.google.code.gson:gson:2.8.9") // For JSON parsing
+    implementation ("com.google.android.libraries.places:places:3.3.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation ("com.google.code.gson:gson:2.8.9")
     implementation("com.airbnb.android:lottie:6.3.0")
     implementation ("com.google.android.gms:play-services-maps:17.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/example/bugbustersproject/MainActivity.java
+++ app/src/main/java/com/example/bugbustersproject/MainActivity.java
@@ -6,11 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -45,8 +40,6 @@ import java.util.concurrent.Executors;
                                 // Access the weather icon directly
                                 int iconCode = weatherInfo.getCode();
                                 weatherPopupWindow.showPopup(iconCode, temperature);
-
-
                             }
                         }
                     });
